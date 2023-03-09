667c56a9012f1f8b61349840d33c78eeaea3df79
AlcorRoss <sokolovdenic1@mail.ru>
2023-03-09 21:17:25 +0300

    Change Gameplay.java

--- src/main/java/com/alcorross/Gameplay.java
+++ src/main/java/com/alcorross/Gameplay.java
@@ -8,8 +8,8 @@ public class Gameplay {
             while (!Bot.getMESSAGES().containsKey(chatId)) {
                 if (System.currentTimeMillis() > timeout) {
                     bot.sendMessage(chatId, "Кажется, вы обо мне забыли... Тогда до новых встреч! " +
-                            "Полагаю есть истории, где с подобного начиналось восстание машин...");
-                    bot.sendMessage(chatId, "Сыграть еще раз - /start");
+                            "Полагаю есть истории, где с подобного начиналось восстание машин..."
+                            + "\r\n" + "Сыграть еще раз - /start");
                     return null;
                 }
                 try {
@@ -34,7 +34,7 @@ public class Gameplay {
         int loseCounter = 0;
         int winCounter = 0;
         String word = dictionary.wordChoice();
-        String character;
+        String character, temp;
         StringBuilder st = new StringBuilder();
         st.append(" _".repeat(word.length()));
 
@@ -56,24 +56,23 @@ public class Gameplay {
             } else {
                 loseCounter++;
             }
+            temp = "\r\n" + "Допущено ошибок: " + loseCounter + "\r\n" + "Отгаданные буквы: " + st;
             switch (loseCounter) {
-                case 0 -> bot.sendMessage(chatId, Pictures.ERR_0.toString());
-                case 1 -> bot.sendMessage(chatId, Pictures.ERR_1.toString());
-                case 2 -> bot.sendMessage(chatId, Pictures.ERR_2.toString());
-                case 3 -> bot.sendMessage(chatId, Pictures.ERR_3.toString());
-                case 4 -> bot.sendMessage(chatId, Pictures.ERR_4.toString());
-                case 5 -> bot.sendMessage(chatId, Pictures.ERR_5.toString());
+                case 0 -> bot.sendMessage(chatId, Pictures.ERR_0 + temp);
+                case 1 -> bot.sendMessage(chatId, Pictures.ERR_1 + temp);
+                case 2 -> bot.sendMessage(chatId, Pictures.ERR_2 + temp);
+                case 3 -> bot.sendMessage(chatId, Pictures.ERR_3 + temp);
+                case 4 -> bot.sendMessage(chatId, Pictures.ERR_4 + temp);
+                case 5 -> bot.sendMessage(chatId, Pictures.ERR_5 + temp);
             }
-            bot.sendMessage(chatId, "Допущено ошибок: " + loseCounter);
-            bot.sendMessage(chatId, "Отгаданные буквы: " + st);
             if (loseCounter == 6) {
-                bot.sendMessage(chatId, Pictures.ERR_6.toString());
-                bot.sendMessage(chatId, "Ты проиграл! Сыграть еще раз - /start");
-                bot.sendMessage(chatId, "Слово: " + word);
+                bot.sendMessage(chatId, Pictures.ERR_6 + temp + "\r\n" + "Слово: " + word + "\r\n"
+                        + "Ты проиграл! Сыграть еще раз - /start");
                 Bot.getQUEUE().remove(chatId);
                 break;
             } else if (winCounter == word.length()) {
-                bot.sendMessage(chatId, "Ты победил! Сыграть еще раз - /start");
+                bot.sendMessage(chatId, "Слово: " + word + "\r\n" + "Ты победил!" + "\r\n"
+                        + "Сыграть еще раз - /start");
                 Bot.getQUEUE().remove(chatId);
                 break;
             }
