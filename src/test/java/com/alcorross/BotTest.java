f613b93d5a19c6ac590f3dacf57cc4bc0e4b730c
AlcorRoss <sokolovdenic1@mail.ru>
2023-03-12 21:41:59 +0300

    Change BotTest.java

--- src/test/java/com/alcorross/BotTest.java
+++ src/test/java/com/alcorross/BotTest.java
@@ -1,20 +1,45 @@
 package com.alcorross;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class BotTest {
+    private static final Bot bot=new Bot();
+    private static final Properties prop=new Properties();
+
+    @BeforeAll
+    static void loadProp() {
+        try (FileInputStream fis = new FileInputStream("src/test/resources/configTest.properties")) {
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void botUsernameShouldNotBeEmpty() {
-        Bot bot = new Bot();
         assertFalse(bot.getBotUsername().isEmpty(), "Bot username should not be empty");
     }
 
     @Test
+    void botUsernameShouldBeEqualsExpectedOne() {
+        assertEquals(bot.getBotUsername(), prop.getProperty("username"),"Username must match the expected one");
+    }
+
+    @Test
     void botTokenShouldNotBeEmpty() {
-        Bot bot = new Bot();
         assertFalse(bot.getBotToken().isEmpty(), "Bot token should not be empty");
+    }
+
+    @Test
+    void botTokenShouldBeEqualsExpectedOne() {
+        assertEquals(bot.getBotToken(), prop.getProperty("token"),"Token must match the expected one");
     }
 }
