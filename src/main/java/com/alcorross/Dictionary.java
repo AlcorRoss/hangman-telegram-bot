92ceb93fd966c1d16ff05f87ecb331fb32ba48fa
AlcorRoss <sokolovdenic1@mail.ru>
2023-03-13 15:02:16 +0300

    Change Dictionary.java

--- src/main/java/com/alcorross/Dictionary.java
+++ src/main/java/com/alcorross/Dictionary.java
@@ -1,6 +1,7 @@
 package com.alcorross;
 
 import lombok.Cleanup;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.BufferedReader;
@@ -13,6 +14,7 @@ import java.util.Random;
 
 @Slf4j
 public class Dictionary {
+    @Getter
     private final static List<String> dictionary = new ArrayList<>();
 
     public void readDictionary() {
