d0745947e0e60086b34898ec58584e93449ae7db
AlcorRoss <sokolovdenic1@mail.ru>
2023-03-13 15:44:19 +0300

    Change .gitignore, add DictionaryTest.java and dictionaryTest.txt

---
+++ src/test/java/com/alcorross/DictionaryTest.java
@@ -0,0 +1,44 @@
+package com.alcorross;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DictionaryTest {
+
+    private static final Dictionary dict = new Dictionary();
+    private static final List<String> testList = new ArrayList<>();
+
+    @BeforeAll
+    static void loadDictionaryTest() {
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader("src/test/resources/dictionaryTest.txt"))) {
+            while ((line = br.readLine()) != null) testList.add(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void listDictionaryShouldBeEqualTestList() {
+        dict.readDictionary();
+        assertEquals(testList, Dictionary.getDictionary(), "The dictionary list must be equal " +
+                "to the testList list");
+    }
+
+    @RepeatedTest(10000)
+    void wordChoiceShouldReturnStringContainedInTheTestList() {
+        String word = dict.wordChoice();
+        assertTrue(testList.contains(word), "The wordChoice should return the word contained" +
+                " in the testList: " + word);
+    }
+}
