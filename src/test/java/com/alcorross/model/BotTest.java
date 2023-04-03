4ca15e6470e01a1b18fc9acb0384a39e2c7cc34a
AlcorRoss <sokolovdenic1@mail.ru>
2023-04-03 17:41:09 +0300

    Change BotTest.java

--- src/test/java/com/alcorross/model/BotTest.java
+++ src/test/java/com/alcorross/model/BotTest.java
@@ -1,11 +1,24 @@
 package com.alcorross.model;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BotTest {
     private final Bot bot = Bot.getBotInstance();
+    private static Update update;
+
+    @BeforeAll
+    static void prepareMessage() {
+        update = Mockito.mock(Update.class);
+        Message message = Mockito.mock(Message.class);
+        Mockito.doReturn(message).when(update).getMessage();
+    }
 
     @Test
     void getBotInstanceShouldReturnBot() {
@@ -16,5 +29,12 @@ class BotTest {
     @Test
     void botUsernameShouldNotBeEmpty() {
         assertThat(bot.getBotUsername()).withFailMessage("Bot username should not be empty").isNotEmpty();
+    }
+
+    @RepeatedTest(50)
+    void onUpdateReceivedShouldIncreaseMessages() {
+        bot.onUpdateReceived(update);
+        assertThat(bot.getMessages()).isNotEmpty();
+        bot.getMessages().clear();
     }
 }
