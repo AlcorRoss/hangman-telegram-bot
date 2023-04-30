f70429d28a7edc42a3a4087f5b4ac25885c63fb6
AlcorRoss <sokolovdenic1@mail.ru>
2023-04-30 21:31:05 +0300

    Change Main.java

--- src/main/java/com/alcorross/Main.java
+++ src/main/java/com/alcorross/Main.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        DBTableCreator.createDBTable(); // запуск liquibase
+        DBTableCreator.createDBTable();
         Bot bot = Bot.getInstance();
         Thread listenerThread = new Thread(Listener.getInstance());
         listenerThread.setDaemon(true);
